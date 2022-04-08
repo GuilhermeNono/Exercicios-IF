d86557606b47f494c858f6cb7bf40f578ae95598
GuilhermeEtec <Guilherme.nono@etec.sp.gov.br>
2022-04-07 21:40:49 -0300

    Primeiro commit

---
+++ ex2/index.js
@@ -0,0 +1,11 @@
+var usuarioNumero = parseInt(prompt('Informe um numero: '))
+var processamento = usuarioNumero % 2
+
+if (processamento == 0) {
+    alert('Par')
+}
+else {
+    alert('Impar')
+}
+
+
---
+++ ex3/index.js
@@ -0,0 +1,11 @@
+var ano = parseInt(prompt('Informe o ano: '))
+
+var anoBissexto = ano % 4
+
+if (anoBissexto == 0) {
+  alert('Ano bissexto')
+}
+else {
+  alert('Ano não bissexto')
+}
+
